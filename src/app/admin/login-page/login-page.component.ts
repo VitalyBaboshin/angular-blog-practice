7586ec26ba74e97caabad283da8ba7150a3ba355
VitalyBaboshin <vitaliy2@ro.ru>
2020-05-18 23:38:03 +0500

    Добвлена переменная submitted для отслеживания ожидания ответа и блокирование на это время кнопки

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -11,10 +11,11 @@ import {Router} from "@angular/router";
 })
 export class LoginPageComponent implements OnInit {
 
-  form: FormGroup
+  form: FormGroup;
+  submitted = false;
 
   constructor(
-    private auth: AuthServices,
+    public auth: AuthServices,
     // для редиректа
     private router: Router
   ) {
@@ -38,6 +39,8 @@ export class LoginPageComponent implements OnInit {
       return
     }
 
+    this.submitted = true;
+
     const user: User = {
       email: this.form.value.email,
       password: this.form.value.password
@@ -46,8 +49,10 @@ export class LoginPageComponent implements OnInit {
     this.auth.login(user).subscribe(() => {
       this.form.reset()
       this.router.navigate(['/admin', 'dashboard'])
+      this.submitted = false;
+    }, error => {
+      this.submitted = false;
     })
-
   }
 }
 
