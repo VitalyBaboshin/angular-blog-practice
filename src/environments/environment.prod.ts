60d84c31a50ecc995398321737d35116b2fcf452
VitalyBaboshin <vitaliy2@ro.ru>
2020-05-19 23:31:13 +0500

    добавлен адрес firebase database

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import {Environment} from "./interface";
 
 export const environment: Environment = {
   production: true,
-  apiKey: 'AIzaSyAnWFf0pv8G9cNiNuNvpQvqjqm6aBATgbc'
+  apiKey: 'AIzaSyAnWFf0pv8G9cNiNuNvpQvqjqm6aBATgbc',
+  fbDbUrl: 'https://angular-blog-practice.firebaseio.com'
 };
