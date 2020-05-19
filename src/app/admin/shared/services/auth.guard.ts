aca85b6e0c21ca8da29824de4bbe2d55c6039d1f
VitalyBaboshin <vitaliy2@ro.ru>
2020-05-19 23:29:20 +0500

    add authGuard

---
+++ src/app/admin/shared/services/auth.guard.ts
@@ -0,0 +1,31 @@
+import {Injectable} from "@angular/core";
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from "@angular/router";
+import {Observable} from "rxjs";
+import {AuthServices} from "./auth.services";
+
+@Injectable()
+export class AuthGuard implements CanActivate{
+
+  //Присутствует ли авторизация сейчас у пользователя
+  constructor(
+    private auth: AuthServices,
+    private router: Router
+  ) {
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot, state: RouterStateSnapshot)
+    : Observable<boolean> | Promise<boolean> | boolean {
+    if (this.auth.isAuthenticated()) {
+      return true
+    } else {
+      this.auth.logout()
+      this.router.navigate(['/admin', 'login'], {
+        queryParams: {
+          loginAgain: true
+        }
+      })
+    }
+  }
+
+}
